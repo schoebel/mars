337738533f0a5da207b4af8463ba08d79552640b
Thomas Schoebel-Theuer <tst@schoebel-theuer.de>
2014-07-10 11:04:48 +0200

    proc: substitute aio bricks with sio for kernel upstream

--- kernel/sy_old/mars_proc.c
+++ kernel/sy_old/mars_proc.c
@@ -36,9 +36,11 @@
 #include "mars_proc.h"
 #include "../lib_mapfree.h"
 #include "../mars_bio.h"
+//      remove_this
 #ifndef __USE_COMPAT
 #include "../mars_aio.h"
 #endif
+//      end_remove_this
 #include "../mars_if.h"
 #include "../mars_copy.h"
 #include "../mars_client.h"
@@ -247,12 +249,14 @@ struct ctl_table io_tuning_table[] = {
 	THRESHOLD_ENTRIES(&bio_submit_threshold, "bio_submit"),
 	THRESHOLD_ENTRIES(&bio_io_threshold[0],  "bio_io_r"),
 	THRESHOLD_ENTRIES(&bio_io_threshold[1],  "bio_io_w"),
+//      remove_this
 #ifndef __USE_COMPAT
 	THRESHOLD_ENTRIES(&aio_submit_threshold, "aio_submit"),
 	THRESHOLD_ENTRIES(&aio_io_threshold[0],  "aio_io_r"),
 	THRESHOLD_ENTRIES(&aio_io_threshold[1],  "aio_io_w"),
 	THRESHOLD_ENTRIES(&aio_sync_threshold,   "aio_sync"),
 #endif
+//      end_remove_this
 	{}
 };
 
@@ -302,9 +306,11 @@ struct ctl_table mars_table[] = {
 	INT_ENTRY("show_debug_messages",  brick_say_debug,        0600),
 	INT_ENTRY("show_statistics_global", global_show_statist,  0600),
 	INT_ENTRY("show_statistics_server", server_show_statist,  0600),
+//      remove_this
 #ifndef __USE_COMPAT
 	INT_ENTRY("aio_sync_mode",        aio_sync_mode,          0600),
 #endif
+//      end_remove_this
 	INT_ENTRY("logger_completion_semantics", trans_logger_completion_semantics, 0600),
 	INT_ENTRY("logger_do_crc",        trans_logger_do_crc,    0600),
 	INT_ENTRY("syslog_min_class",     brick_say_syslog_min,   0600),
