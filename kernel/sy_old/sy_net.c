schoebel/mars

commit 1ca8d711ffebc570d23fc4b37e347762dbe156d6

infra: TRANSITIONAL base hostname translation directly on symlinks
This is needed on modprobe when no dents already exist.
Long-term solution will be very different.

kernel/sy_old/sy_net.c
@@ -36,14 +36,9 @@
 static
 char *_mars_translate_hostname(const char *name)
 {
-	struct mars_global *global = mars_global;
 	char *res = brick_strdup(name);
-	struct mars_dent *test;
+	char *test;
 	char *tmp;
-
-	if (unlikely(!global)) {
-		goto done;
-	}
 
 	for (tmp = res; *tmp; tmp++) {
 		if (*tmp == ':') {
@@ -57,13 +52,13 @@ char *_mars_translate_hostname(const char *name)
 		goto done;
 	}
 
-	test = mars_find_dent(global, tmp);
-	if (test && test->new_link) {
-		MARS_DBG("'%s' => '%s'\n", tmp, test->new_link);
+	test = mars_readlink(tmp);
+	if (test && test[0]) {
+		MARS_DBG("'%s' => '%s'\n", tmp, test);
 		brick_string_free(res);
-		res = brick_strdup(test->new_link);
+		res = test;
 	} else {
-		MARS_DBG("no translation for '%s'\n", tmp);
+		MARS_WRN("no hostname translation for '%s'\n", tmp);
 	}
 	brick_string_free(tmp);
 

commit 444a60defa5ee487dac79b371313ac6e7c85a616

net: fix memory leak
This was introduced in 1ca8d711ffebc570d23fc4b37e347762dbe156d6

kernel/sy_old/sy_net.c
@@ -58,6 +58,7 @@ char *_mars_translate_hostname(const char *name)
 		brick_string_free(res);
 		res = test;
 	} else {
+		brick_string_free(test);
 		MARS_WRN("no hostname translation for '%s'\n", tmp);
 	}
 	brick_string_free(tmp);