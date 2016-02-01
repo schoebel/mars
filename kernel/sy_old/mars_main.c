0523705eb6e7c80b8bca975dbe83f04846df1863
Thomas Schoebel-Theuer <tst@schoebel-theuer.de>
2016-02-01 08:38:29 +0100

    main: report flying requests at if

--- kernel/sy_old/mars_main.c
+++ kernel/sy_old/mars_main.c
@@ -4786,6 +4786,8 @@ void _show_dev(struct mars_rotate *rot)
 
 	if (if_brick) {
 		_show_rate(rot, &if_brick->io_limiter, "if_rate");
+		__show_actual(rot->parent_path, "if-flying",
+			      atomic_read(&if_brick->flying_count));
 		open_count = atomic_read(&if_brick->open_count);
 	}
 	__show_actual(rot->parent_path, "open-count", open_count);
