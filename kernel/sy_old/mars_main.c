schoebel/mars

commit e969219fca0669e611d79b4a3f71ec0fe5d2bba5

main: safeguard versionlink appearance
In some rare cases (e.g. damaged /mars or crashed primaries),
the versionlink belonging to a  logfile may be missing.

Don't insist on the existence of a versionlink if the logfile is
stemming from myself (automatic self-repair).

kernel/sy_old/mars_main.c
@@ -2611,7 +2611,7 @@ bool is_switchover_possible(struct mars_rotate *rot, const char *old_log_path, c
 		make_rot_msg(rot, "err-versionlink-not-readable", "cannot read old versionlink '%s'", SAFE_STR(old_versionlink_path));
 		goto done;
 	}
-	if (!skip_new) {
+	if (!skip_new && strcmp(new_host, my_id())) {
 		new_versionlink = get_versionlink(rot->parent_path, new_log_seq, new_host, &new_versionlink_path);
 		if (unlikely(!new_versionlink || !new_versionlink[0])) {
 			MARS_INF_TO(rot->log_say, "new versionlink '%s' does not yet exist, we must wait for it.\n", SAFE_STR(new_versionlink_path));

commit b04db9a5ef7c462dacbf033dcaff84da7647463f

main: fix NULL pointer deref
Regression from e969219fca0669e611d79b4a3f71ec0fe5d2bba5

kernel/sy_old/mars_main.c
@@ -2680,7 +2680,7 @@ bool is_switchover_possible(struct mars_rotate *rot, const char *old_log_path, c
 	}
 
 	// last check: is the new versionlink based on the old one?
-	if (!skip_new) {
+	if (new_versionlink) {
 		len1  = skip_sect(own_versionlink);
 		offs2 = skip_sect(new_versionlink);
 		if (unlikely(!new_versionlink[offs2++])) {