79e35c3eb06b9db63afdc1d5ee8236ba64a42245
Thomas Schoebel-Theuer <tst@1und1.de>
2026-07-23 11:01:24 +0200

    main: tighten check

--- kernel/sy_old/mars_main.c
+++ kernel/sy_old/mars_main.c
@@ -2702,6 +2702,7 @@ int _update_file(struct mars_dent *parent, const char *switch_path, const char *
 		if (do_start)
 			rot->retry_log_from = 0;
 		if (fetch_brick->copy_last != rot->old_copy_last &&
+		    fetch_brick->power.button &&
 		    fetch_brick->power.led_on) {
 			rot->old_copy_last = fetch_brick->copy_last;
 			rot->fetch_jiffies = jiffies;
